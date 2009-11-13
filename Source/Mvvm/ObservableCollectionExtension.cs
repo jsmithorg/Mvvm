28be34cbdb34fd64508b74352b579d8407180f29
Justin Smith <justin@jsmith.org>
2009-11-13 15:02:32 -0800

    [feature] added ToObservableCollection extension method

---
+++ Source/Mvvm/ObservableCollectionExtension.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+
+namespace JSmith.Mvvm
+{
+    public static class ObservableCollectionExtension
+    {
+        public static ObservableCollection<T> ToObservableCollection<T>(this IEnumerable<T> enumerableList)
+        {
+            if (enumerableList != null)
+            {
+                //create an emtpy observable collection object
+                ObservableCollection<T> observableCollection = new ObservableCollection<T>();
+
+                //loop through all the records and add to observable collection object
+                foreach (var item in enumerableList)
+                    observableCollection.Add(item);
+
+                //return the populated observable collection
+                return observableCollection;
+
+            }//end if
+
+            return null;
+
+        }//end method
+
+    }//end class
+
+}//end namespace
