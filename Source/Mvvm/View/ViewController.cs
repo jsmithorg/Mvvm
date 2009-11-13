bbe657dd3e091066869e2226fe69f43d354d5cf4
Justin Smith <justin.smith@glg.com>
2009-11-12 17:54:57 -0800

    [feature] base view controller class added for managing views

---
+++ Source/Mvvm/View/ViewController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using JSmith.Mvvm.View;
+using System.Windows.Markup;
+
+namespace JSmith.Mvvm.View
+{
+    public class ViewController : Control
+    {
+        private IView _view;
+        public IView View
+        {
+            get { return _view; }
+            set
+            {
+                if (_view == value)
+                    return;
+
+                _view = value;
+
+                //do transition
+
+                _root.Children.Clear();
+                _root.Children.Add((UIElement)_view);
+
+            }//end set
+
+        }//end property
+
+        private Grid _root;
+
+        public ViewController()
+        {
+            string controlTemplate = @"
+                <ControlTemplate xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation""
+                                 xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml"" TargetType=""Control"">
+                    <Grid x:Name=""LayoutRoot"" />
+                </ControlTemplate>";
+
+            Template = (ControlTemplate)XamlReader.Load(controlTemplate);
+
+            ApplyTemplate();
+
+        }//end constructor
+
+        public override void OnApplyTemplate()
+        {
+            _root = (Grid)GetTemplateChild("LayoutRoot");
+
+            base.OnApplyTemplate();
+
+        }//end method
+
+    }//end class
+
+}//end namespace
