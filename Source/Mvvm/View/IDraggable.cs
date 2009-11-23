337653a691c9d489d695fd170746bff86324fdd2
Justin Smith <justin@jsmith.org>
2009-11-23 13:43:31 -0800

    [feature] added IDraggable interface for draggable views

---
+++ Source/Mvvm/View/IDraggable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace JSmith.Mvvm.View
+{
+    public interface IDraggable
+    {
+        event EventHandler DragStarted;
+        event MouseEventHandler Dragging;
+        event EventHandler DragComplete;
+
+        void BeginDrag();
+        void EndDrag();
+
+    }//end interface
+
+}//end namespace
