cf58611ced03d1b52fa0d158a8506d7d38a2ae4e
Justin Smith <justin@jsmith.org>
2009-11-23 15:15:21 -0800

    [feature] added example for draggable views

---
+++ Examples/DragExample/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using JSmith.Mvvm.View;
+
+namespace DragExample
+{
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            MyDragView.DragStarted += new EventHandler(MyDragView_DragStarted);
+            MyDragView.Dragging += new MouseEventHandler(MyDragView_Dragging);
+            MyDragView.DragComplete += new EventHandler(MyDragView_DragComplete);
+        }
+
+        void MyDragView_DragComplete(object sender, EventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("DRAQ COMPLETE");
+        }
+
+        void MyDragView_Dragging(object sender, MouseEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("DRAGGING");
+
+            DragView dv = (DragView)sender;
+            dv.SetValue(Canvas.LeftProperty, e.GetPosition(null).X - (dv.Width / 2));
+            dv.SetValue(Canvas.TopProperty, e.GetPosition(null).Y - (dv.Height / 2));
+        }
+
+        void MyDragView_DragStarted(object sender, EventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("DRAQ STARTED");
+        }
+    }
+}
